d7e5d7614519bfd695b8f94f4c41483a6ecd6b1c
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-11-10 04:19:58 +0100

    init appos theme

---
+++ src/appos/template-overview.php
@@ -0,0 +1,28 @@
+<div class="template-preview">
+	<img src="<?php echo $gantry->templateUrl;?>/template-thumb-big.png" style="max-width:323px;" />
+	<h2>Key Features</h2>
+	<ul>
+		<li>Responsive Design supporting Phone, Tablets and Desktops</li>
+		<li>LESS CSS auto-compilation</li>
+		<li>Flexible widgets for template customization</li>
+		<li>Nawala Rapid Development Kit with full extensible framework architecture</li>
+		<li>XML driven and with overrides for unprecedented levels of customization</li>
+		<li>Per menu-item level of control over any configuration option with inheritance</li>
+	</ul>
+</div>
+<div class="template-description">
+	<h1><?php echo $gantry->get('template_full_name');?> <span class="g4-version">v<?php echo $gantry->_template->getVersion();?></span></h1>
+	<h2>IRM Application Optimized Theme</h2>
+
+	<p>The IRM Application theme is a clean modern responsive design that is a great place to start when building your custom Gantry powered theme.</p>
+
+	<h2>What is the Nawala Rapid Development Kit?</h2>
+
+	<p>The Nawala!RDK is a sophisticated framework with the sole intention of being the best platform to build solid web apps with.
+	    Nawala takes all the lessons learned during the development of many XiveApp's and XiveTheme's and distills that knowledge
+	    into a single super-flexible environment that is easy to configure, simple to extend, and powerful enough to handle anything
+	    we want to throw at it.</p>
+
+	<p>Get more help and find out more at <a href="http://devxive.com">http://devxive.com</a></p>
+
+</div>
