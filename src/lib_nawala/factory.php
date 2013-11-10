6d747a5b3e1bef5f2af3081c31c95d84ddd0d51c
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-11-10 12:18:39 +0100

    moved adSyyle and adScript methods to factory class

--- src/lib_nawala/factory.php
+++ src/lib_nawala/factory.php
@@ -19,7 +19,84 @@ defined('_NRDKRA') or die;
  */
 abstract class NFWFactory
 {
-	function test() {
-		echo 'it works';
+//	include( JPATH_SITE . '/libraries/gantry/gantry.php' );
+
+	/**
+	 * Adds a script file to the document with platform based checks
+	 *
+	 * @param  $file
+	 *
+	 * @return void
+	 */
+	function nawala_addScript($file)
+	{
+		gantry_import('core.gantryplatform');
+		$platform      = new GantryPlatform();
+		$document      = JFactory::getDocument();
+		$filename      = basename($file);
+		$relative_path = dirname($file);
+
+		// For local url path get the local path based on checks
+		$file_path       = gantry_getFilePath($file);
+		$url_file_checks = $platform->getJSChecks($file_path, true);
+		foreach ($url_file_checks as $url_file) {
+			$full_path = realpath($url_file);
+			if ($full_path !== false && file_exists($full_path)) {
+				$document->addScript($relative_path . '/' . basename($full_path) . '?ver=' . NAWALA_VERSION);
+				break;
+			}
+		}
+	}
+
+	/**
+	 * Add inline script to the document
+	 *
+	 * @param  $script
+	 *
+	 * @return void
+	 */
+	function nawala_addInlineScript($script)
+	{
+		$document = JFactory::getDocument();
+		$document->addScriptDeclaration($script);
+	}
+
+	/**
+	 * Add a css style file to the document with browser based checks
+	 *
+	 * @param  $file
+	 *
+	 * @return void
+	 */
+	function nawala_addStyle($file)
+	{
+		gantry_import('core.gantrybrowser');
+		$browser       = new GantryBrowser();
+		$document      = JFactory::getDocument();
+		$filename      = basename($file);
+		$relative_path = dirname($file);
+
+		// For local url path get the local path based on checks
+		$file_path       = gantry_getFilePath($file);
+		$url_file_checks = $browser->getChecks($file_path, true);
+		foreach ($url_file_checks as $url_file) {
+			$full_path = realpath($url_file);
+			if ($full_path !== false && file_exists($full_path)) {
+				$document->addStyleSheet($relative_path . '/' . basename($full_path) . '?ver=' . NAWALA_VERSION);
+			}
+		}
+	}
+
+	/**
+	 * Add inline css to the document
+	 *
+	 * @param  $css
+	 *
+	 * @return void
+	 */
+	function nawala_addInlineStyle($css)
+	{
+		$document = JFactory::getDocument();
+		$document->addStyleDeclaration($css);
 	}
 }
--- src/lib_nawala/nawala.php
+++ src/lib_nawala/nawala.php
@@ -26,7 +26,7 @@ if (!defined('NAWALA_VERSION')) {
 		define('DS', DIRECTORY_SEPARATOR);
 	}
 
-	define('NAWALA_LIBRARY', JPATH_LIBRARIES . '/nawala');
+	define( 'NAWALA_LIBRARY', JPATH_LIBRARIES . '/nawala' );
 
 	// Register the library.
 //	JLoader::registerPrefix('Nawala', JPATH_LIBRARIES . '/nawala');
@@ -36,93 +36,6 @@ if (!defined('NAWALA_VERSION')) {
 	// Init the factory if necessary.
 	if (!class_exists('NFactory'))
 	{
-		require_once(NAWALA_LIBRARY . '/factory.php');
-	}
-
-	// Load gantry class
-	$gantry_lib_path = JPATH_SITE . '/libraries/gantry/gantry.php';
-	if (!file_exists($gantry_lib_path)) {
-	    echo JText::_('GANTRY_BOOTSTRAP_CANT_FIND_LIBRARY');
-	    die;
-	}
-	include($gantry_lib_path);
-
-	/**
-	 * Adds a script file to the document with platform based checks
-	 *
-	 * @param  $file
-	 *
-	 * @return void
-	 */
-	function nawala_addScript($file)
-	{
-		gantry_import('core.gantryplatform');
-		$platform      = new GantryPlatform();
-		$document      = JFactory::getDocument();
-		$filename      = basename($file);
-		$relative_path = dirname($file);
-
-		// For local url path get the local path based on checks
-		$file_path       = gantry_getFilePath($file);
-		$url_file_checks = $platform->getJSChecks($file_path, true);
-		foreach ($url_file_checks as $url_file) {
-			$full_path = realpath($url_file);
-			if ($full_path !== false && file_exists($full_path)) {
-				$document->addScript($relative_path . '/' . basename($full_path) . '?ver=' . NAWALA_VERSION);
-				break;
-			}
-		}
-	}
-
-	/**
-	 * Add inline script to the document
-	 *
-	 * @param  $script
-	 *
-	 * @return void
-	 */
-	function nawala_addInlineScript($script)
-	{
-		$document = JFactory::getDocument();
-		$document->addScriptDeclaration($script);
-	}
-
-	/**
-	 * Add a css style file to the document with browser based checks
-	 *
-	 * @param  $file
-	 *
-	 * @return void
-	 */
-	function nawala_addStyle($file)
-	{
-		gantry_import('core.gantrybrowser');
-		$browser       = new GantryBrowser();
-		$document      = JFactory::getDocument();
-		$filename      = basename($file);
-		$relative_path = dirname($file);
-
-		// For local url path get the local path based on checks
-		$file_path       = gantry_getFilePath($file);
-		$url_file_checks = $browser->getChecks($file_path, true);
-		foreach ($url_file_checks as $url_file) {
-			$full_path = realpath($url_file);
-			if ($full_path !== false && file_exists($full_path)) {
-				$document->addStyleSheet($relative_path . '/' . basename($full_path) . '?ver=' . NAWALA_VERSION);
-			}
-		}
-	}
-
-	/**
-	 * Add inline css to the document
-	 *
-	 * @param  $css
-	 *
-	 * @return void
-	 */
-	function nawala_addInlineStyle($css)
-	{
-		$document = JFactory::getDocument();
-		$document->addStyleDeclaration($css);
+		require_once( NAWALA_LIBRARY . '/factory.php' );
 	}
 }
