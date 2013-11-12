f87ec8f9e7025c40669c9e523362dadfcc5e3680
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-11-12 01:18:49 +0100

    update appos template overview descriptions

--- src/appos/template-overview.php
+++ src/appos/template-overview.php
@@ -1,3 +1,9 @@
+<?php
+defined( '_JEXEC' ) or die;
+
+// Create the Templates XML Object
+$nxml = new NCoreSimplexml(JPATH_ROOT . '/templates/appos/templateDetails.xml', null, true);
+?>
 <div class="template-preview">
 	<img src="<?php echo $gantry->templateUrl;?>/template-thumb-big.png" style="max-width:323px;" />
 	<h2>Key Features</h2>
@@ -11,18 +17,23 @@
 	</ul>
 </div>
 <div class="template-description">
-	<h1><?php echo $gantry->get('template_full_name');?> <span class="g4-version">v<?php echo $gantry->_template->getVersion();?></span></h1>
-	<h2>IRM Application Optimized Theme</h2>
+	<h1><?php echo JText::_($nxml->get('name')); ?> <span class="g4-version">v<?php echo $nxml->get('version'); ?></span></h1>
+	<h2>Application Optimized Theme</h2>
 
-	<p>The IRM Application theme is a clean modern responsive design that is a great place to start when building your custom Gantry powered theme.</p>
+	<p><?php echo JText::_($nxml->get('name')); ?> is a clean modern responsive design that is especially build and optimized for web applications.</p>
 
-	<h2>What is the Nawala Rapid Development Kit?</h2>
+	<h2>What is the Nawala!RDK?</h2>
 
-	<p>The Nawala!RDK is a sophisticated framework with the sole intention of being the best platform to build solid web apps with.
-	    Nawala takes all the lessons learned during the development of many XiveApp's and XiveTheme's and distills that knowledge
-	    into a single super-flexible environment that is easy to configure, simple to extend, and powerful enough to handle anything
-	    we want to throw at it.</p>
+	<p>
+		The Nawala!RDK (Rapid Development Kit) is a sophisticated set of powerful frameworks and applications with the sole intention of
+		being the best platform to build solid web apps with.<br>
+		<br>
+		Nawala takes all the lessons learned during the development of many XiveApp's and XiveTheme's and distills that knowledge into a
+		single super-flexible environment that is easy to configure, simple to extend, and powerful enough to handle anything we want to
+		throw at it.
+	</p>
 
-	<p>Get more help and find out more at <a href="http://devxive.com">http://devxive.com</a></p>
+	<p>Get help and find out more at <a href="http://devxive.com" target="_blank">http://devxive.com</a></p>
 
+	<p class="text-right text-stroke"><small>Last updated: <?php echo JHTML::_( 'date', $nxml->get('creationDate'), JText::_('DATE_FORMAT_LC') ); ?></small></p>
 </div>
