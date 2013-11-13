ccee652247d5dc6e5ea473a49fb0e58c9ebf9259
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-11-13 20:13:34 +0100

    add compressor to addstyle, add method descriptions

--- src/lib_nawala/gantry/helper.php
+++ src/lib_nawala/gantry/helper.php
@@ -38,9 +38,21 @@ class NGantryHelper
 	static $urlPath;
 
 	/**
+	 * array    $pathArray    Array of paths methods and functions will look into to find the appropriate files
+	 *
 	 * @var
 	 */
 	static $pathArray;
+
+	/**
+	 * @var
+	 */
+	static $lessCompression;
+
+	/**
+	 * @var
+	 */
+	static $cssCompression;
 
 
 	/**
@@ -48,6 +60,9 @@ class NGantryHelper
 	 */
 	public function __construct()
 	{
+		// Initialize global gantry
+		global $gantry;
+
 		$this->template = NCore::get('template')->name;
 		$this->templatePath = NAWALA_BASEPATH_FILESYSTEM . '/templates/' . $this->template;
 		$this->urlPath = NAWALA_BASEPATH_URL;
@@ -57,20 +72,33 @@ class NGantryHelper
 			'libraries/nawala/assets/less',
 			'libraries/nawala/assets/css'
 		);
+
+		$this->lessCompression = (string) $gantry->get('less-compression', true);
+		$this->cssCompression = (string) $gantry->get('css-compression', false);
 	}
 
 
 	/**
-	 * Method to ...
+	 * Import and compile less files
 	 *
-	 * @param     string    $var    Description
+	 * @param     mixed      $file                       Filename or array of filenames to import. Looks first in templates less folder, then in nawala library assets/less folder
+	 * @param     string     $compiledName               Filename of the compiled file in templates css-compiled folder, if set to false it will generate a md5 dynamic name based on the imported files
+	 * @param     int        $priority                   Priority of the file. Should determine on which position file will be rendered/loaded
+	 * @param     array      $template_files_override    Add conditional style variables to less which will be also compiled: see http://leafo.net/lessphp/docs/#setting_variables_from_php for more informations
+	 * @param     boolean    $compress                   Enable the compressor to read file content and build a compressed one in the templates css-compiled folder.
+	 *                                                   Note: If in template settings CSSmin.YUI is set to "ON", this option will not have any effect!. This can only be used to override if template option is set to "OFF"
 	 *
 	 * @return    void
 	 */
-	public function addStyle( $file = '', $compiledName = false, $priority = self::DEFAULT_STYLE_PRIORITY, $template_files_override = false )
+	public function addStyle( $file = '', $compiledName = false, $priority = self::DEFAULT_STYLE_PRIORITY, $template_files_override = false, $compress = false )
 	{
 		// Initialize global gantry
 		global $gantry;
+
+		if ( $this->cssCompression || $compress ) {
+			require_once(NAWALA_BASEPATH_FILESYSTEM . '/libraries/nawala/compiler/cssmin.yui.php');
+			$compressor = new CSSmin();
+		}
 
 		// Find the file in the appropriate pathArray
 		if ( !$fileIn = JPath::find($this->pathArray, $file) ) {
@@ -89,48 +117,62 @@ class NGantryHelper
 			$fileExtension = JFile::getExt($file);
 			$md5 = md5($file);
 			
-			$compiledName = $fileName . '-' . $md5 . '.' . $fileExtension;
+			if ( $this->cssCompression || $compress ) {
+				$compiledName = $fileName . '-compressed-' . $md5 . '.' . $fileExtension;
+			} else {
+				$compiledName = $fileName . '-' . $md5 . '.' . $fileExtension;
+			}
 		}
 
 		$fileOut = $this->templatePath . '/css-compiled/' . $compiledName;
 
 		if ( !is_file($fileOut) || filemtime($fileIn) > filemtime($fileOut) ) {
 			if ( is_file($fileOut) ) {
-//				JFile::delete( $fileOut );
+				JFile::delete( $fileOut );
 			}
-			JFile::copy( $fileIn, $fileOut );
+			if ( $this->cssCompression || $compress ) {
+				// Read files content
+				$cssBuffer = $compressor->run( JFile::read($fileIn) );
+
+				// Write file
+				JFile::write($fileOut, $cssBuffer);
+			} else {
+				JFile::copy( $fileIn, $fileOut );
+			}
 		}
 
-		$gantry->addStyle($fileOut, $priority, $template_files_override);
+		$fileUrl = $this->urlPath . '/templates/' . $this->template . '/css-compiled/' . $compiledName;
+
+		$gantry->addStyle($fileUrl, $priority, $template_files_override);
 	}
 
 
 	/**
 	 * Import and compile less files
 	 *
-	 * @param     mixed      $files                      Filename or array of filenames to import. Looks first in templates less folder, then in nawala library assets/less folder
-	 * @param     string     $compiledName               Filename of the compiled file in templates css-compiled folder, if set to false it will generate a md5 dynamic name based on the imported files
-	 * @param     int        $priority                   Priority of the file. Should determine on which position file will be rendered/loaded
-	 * @param     boolean    $compress                   Compress the file with cssMinifyer?
-	 * @param     boolean    $template_files_override    ...
+	 * @param     mixed     $files            Filename or array of filenames to import. Looks first in templates less folder, then in nawala library assets/less folder
+	 * @param     string    $compiledName     Filename of the compiled file in templates css-compiled folder, if set to false it will generate a md5 dynamic name based on the imported files
+	 * @param     int       $priority         Priority of the file. Should determine on which position file will be rendered/loaded
+	 * @param     array     $lessVariables    Add conditional style variables to less which will be also compiled: see http://leafo.net/lessphp/docs/#setting_variables_from_php for more informations
 	 *
 	 * @return    void
 	 */
-	public function importLess( $files, $compiledName = false, $priority = self::DEFAULT_STYLE_PRIORITY, $lessVariables = false, $compress = false, $template_files_override = false )
+	public function importLess( $files, $compiledName = false, $priority = self::DEFAULT_STYLE_PRIORITY, array $lessVariables = array() )
 	{
 		// Initialize global gantry
 		global $gantry;
+		$template_files_override = false; // Not relevant in importLess method
 
 		// Require Lessc
 		require_once(NAWALA_BASEPATH_FILESYSTEM . '/libraries/nawala/compiler/lessc.inc.php');
 		$lessc = new lessc;
 		$lessc->setImportDir( $this->pathArray );
 
-		if ( $lessVariables ) {
+		if ( !empty($lessVariables) ) {
 			$lessc->setVariables($lessVariables);
 		}
 
-		if ( $compress ) {
+		if ( $this->lessCompression ) {
 			require_once(NAWALA_BASEPATH_FILESYSTEM . '/libraries/nawala/compiler/cssmin.yui.php');
 			$compressor = new CSSmin();
 		}
@@ -167,10 +209,10 @@ class NGantryHelper
 		}
 
 		if ( !$compiledName ) {
-			if ( $compress ) {
-				$compiledName = 'compressed-' . md5($content) . '.css';
+			if ( $this->lessCompression ) {
+				$compiledName = 'less-compressed-' . md5($content) . '.css';
 			} else {
-				$compiledName = 'compiled-' . md5($content) . '.css';
+				$compiledName = 'less-compiled-' . md5($content) . '.css';
 			}
 		}
 
@@ -190,7 +232,7 @@ class NGantryHelper
 			foreach ( $cache as $file ) {
 				if ( $file['timestamp'] > filemtime($fileOut) ) {
 					$create = true;
-//					JFile::delete( $fileOut );
+					JFile::delete( $fileOut );
 				}
 			}
 		} else {
@@ -199,7 +241,7 @@ class NGantryHelper
 		}
 
 		if ( $create ) {
-			if ( $compress ) {
+			if ( $this->lessCompression ) {
 				// Use cssMin to minify css output
 				$cssBuffer = $compressor->run( $lessc->compile( $content ) );
 			} else {
