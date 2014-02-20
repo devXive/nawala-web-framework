766843afa753d86282e23a6202cc00e26af8db2d
Lahmizzar Muinela <lahmizzar@devxive.com>
2014-02-20 14:40:58 +0100

    init

--- src/lib_nawala/assets/js/jquery-noconflict.js
+++ src/lib_nawala/assets/js/jquery-noconflict.js
@@ -1,6 +1,9 @@
 /*
- * @author    RocketTheme http://www.rockettheme.com
- * @copyright Copyright (C) 2007 - 2013 RocketTheme, LLC
- * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
+ * @package          Nawala Rapid Development Kit
+ * @subPackage       Nawala - Library
+ * @author           devXive - research and development <support@devxive.com> (http://www.devxive.com)
+ * @copyright        Copyright (C) 1997 - 2014 devXive - research and development. All rights reserved.
+ * @license          GNU General Public License version 2 or later; see LICENSE.txt
+ * @assetsLicense    devXive Proprietary Use License (http://www.devxive.com/license)
  */
 jQuery.noConflict();
