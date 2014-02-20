910e346a05d270e4a64504a16c8d588f15aaa8fa
Lahmizzar Muinela <lahmizzar@devxive.com>
2014-02-20 13:55:16 +0100

    minor foxes in NSession class

--- src/lib_nawala/session/session.php
+++ src/lib_nawala/session/session.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package          Nawala Rapid Development Kit
- * @subPackage	Nawala - Library
+ * @subPackage       Nawala - Library
  * @author           devXive - research and development <support@devxive.com> (http://www.devxive.com)
  * @copyright        Copyright (C) 1997 - 2013 devXive - research and development. All rights reserved.
  * @license          GNU General Public License version 2 or later; see LICENSE.txt
@@ -14,18 +14,46 @@ defined('_NRDKRA') or die;
 /**
  * Nawala Framework Session Class
  *
- * This class allows for ???? TODO: Description
- *
  * @package       Framework
- * @subpackage    Core
+ * @subpackage    Session
  * @since         1.0
+ * 
+ * @deprecated    1.1
  */
 class NSession
 {
 	/**
 	 * @var
 	 */
-	static $instance;
+	private $instance;
+
+	/**
+	 * @var    object    $object    Empty object with getters and setters and error
+	 */
+	private $appObject;
+
+	/**
+	 * @var    object    $object    Empty object with getters and setters and error
+	 */
+	private $docObject;
+
+	/**
+	 * @var    object    $object    Empty object with getters and setters and error
+	 */
+	private $tmpObject;
+
+	/**
+	 * @var
+	 */
+	protected $namespace;
+
+	/**
+	 * Global session object
+	 *
+	 * @var    NFactory::getSession
+	 * @since  1.0
+	 */
+	private $session = null;
 
 
 	/**
@@ -33,54 +61,127 @@ class NSession
 	 */
 	public function __construct()
 	{
-		$session = JFactory::getSession();
+			// Get the global namespace
+		$this->namespace = NFactory::getConfig()->getPlatform()->name;
 
-		$nawalaSessionObject = new JObject;
+		if ( !$this->appObject ) {
+			$this->appObject = new JObject();
+		}
 
-		if ( !$session->get('nawala') ) {
-			$session->set('nawala', $nawalaSessionObject);
+		if ( !$this->docObject ) {
+			$this->docObject = new JObject();
+		}
+
+		if ( !$this->tmpObject ) {
+			$this->tmpObject = new JObject();
+		}
+
+		// Initialize and prepare the structure
+		$this->init();
+	}
+
+
+	/**
+	 * Initialize session (used in construct)
+	 */
+	private function init() {
+		if (!$this->session)
+		{
+			$this->session = JFactory::getSession();
+		}
+
+		/*
+		 * Nawala Application Scope
+		 */
+		if ( !$this->session->has('app', $this->namespace) )
+		{
+			$app = $this->appObject;
+		
+			$this->session->set('app', $app, $this->namespace);
+		}
+
+		/*
+		 * Nawala Document Scope
+		 */
+		if ( !$this->session->has('doc', $this->namespace) )
+		{
+			$doc = $this->docObject;
+		
+			$this->session->set('doc', $doc, $this->namespace);
+		}
+
+		/*
+		 * Nawala Temp Scope
+		 */
+		if ( !$this->session->has('tmp', $this->namespace) )
+		{
+			$tmp = $this->tmpObject;
+		
+			$this->session->set('tmp', $tmp, $this->namespace);
 		}
 	}
 
 
 	/**
-	 * Get data from session object
+	 * Get data from the session store
+	 *
+	 * @param   string  $name       Name of a variable
+	 * @param   mixed   $default    Default value of a variable if not set
+	 * @param   string  $namespace  Namespace to use, default to 'default'
+	 *
+	 * @return  mixed  Value of a variable
+	 *
+	 * @since   1.1
 	 */
-	static function get( $data = false )
+	public function get($name, $default = null, $namespace = false)
 	{
-		$session = JFactory::getSession();
-		$nawalaSessionObject = $session->get('nawala');
-
-		if ( $data ) {
-			return $nawalaSessionObject->get((string)$data, null);
-		} else {
-			return $nawalaSessionObject;
+		// Use sessionNamespace if namespace is not set
+		if ( !$namespace ) {
+			$namespace = $this->sessionNamespace;
 		}
+
+		return $this->session->get($name, $default, $namespace);
 	}
 
 
 	/**
-	 * Update session object
+	 * Set data into the session store.
+	 *
+	 * @param   string  $name       Name of a variable.
+	 * @param   mixed   $value      Value of a variable.
+	 * @param   string  $namespace  Namespace to use, default to 'default'.
+	 *
+	 * @return  void
+	 *
+	 * @since   1.1
 	 */
-	public function set( $string = false, $value = false )
+	public function set($name, $value = null, $namespace = false)
 	{
-		$session = JFactory::getSession();
-		$nawalaSessionObject = $session->get('nawala');
-
-		if ( $string && $value ) {
-			$nawalaSessionObject->set($string, $value);
+		// Use sessionNamespace if namespace is not set
+		if( !$namespace ) {
+			$namespace = $this->sessionNamespace;
 		}
+
+		$this->session->set($name, $value, $namespace);
 	}
 
 
 	/**
-	 * Simple function to check if the nawala session object exist or not
+	 * Simple function to check if the given session item exist or not
+	 * 
+	 * @param   string  $name       Name of session item
+	 * @param   string  $namespace  Optional namespace to store in the session item. Useful to avoid conflicts for multiple extensions
+	 * 
+	 * @return boolean
 	 */
-	public function exist()
+	public function exist($name, $namespace = false)
 	{
-		$session = JFactory::getSession();
+		// Use sessionNamespace if namespace is not set
+		if( !$namespace ) {
+			$namespace = $this->sessionNamespace;
+		}
 
-		if ( $session->get('nawala') ) {
+		if ( $this->session->has($name, $namespace) ) {
 			return true;
 		} else {
 			return false;
